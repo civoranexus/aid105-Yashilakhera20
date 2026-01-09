81b3bff0d34c29588cf811d6fde26c001a5daab2
Yashilakhera20 <yashilakhera16@gmail.com>
2026-01-09 17:47:21 +0530

    I resolved the backend password issue and create 2 more file of frontend which is login and main

---
+++ .github/backend/frontend/login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Login({ setToken, setRole }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    const formData = new URLSearchParams();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Invalid credentials");
+      }
+
+      const data = await response.json();
+      setToken(data.access_token);
+      setRole(data.role);
+    } catch (err) {
+      setError("Login failed. Check username or password.");
+    }
+  };
+
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h2>SchemeAssist AI – Login</h2>
+
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+}
