0f4799fdbbd011b24a5c383fe34404d34d336d2e
Yashilakhera20 <yashilakhera16@gmail.com>
2026-01-16 01:27:44 +0530

    I have successfully designed and implemented the initial user flow of my GovTech web application Gov-Yojnaarthi (Your Digital Companion for Government Schemes).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/Landing";
 import UserType from "./pages/UserType";
 import Login from "./pages/Login";
 import "./App.css";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
       <Route path="/user-type" element={<UserType />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/signup" element={<Signup />} />
+
     </Routes>
   );
 }
 
 export default App;
+
